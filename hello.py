df56d06ac262edfab5863734ce11d2f5495b130b
Mythrean-S-R <sit24cs197@sairamtap.edu.in>
2025-04-18 21:54:52 +0530

    dependencies2

--- hello.py
+++ hello.py
@@ -1,34 +1,33 @@
 import streamlit as myth
-import pandas as pd 
-from pygame import mixer
+import pandas as pd
+import numpy as np
+import altair as alt
 from streamlit_option_menu import option_menu
-
-mixer.init()
-
-
-mixer.music.load("assests_music\\RAINING IN ＮＡＧＯＹＡ (Lofi HipHop).mp3")
-mixer.music.play()
-
 import time
+
 myth.markdown(
     "<hr><h1 style='text-align: center; color: teal;'>TEAM XD</h1><hr>", 
     unsafe_allow_html=True
 )
+
 myth.title("TEAM XD")
 myth.write(":rainbow[hary is the owner of SRM]")
+
+# ✅ Streamlit-native audio player (works on cloud)
+with open("assests_music/RAINING IN ＮＡＧＯＹＡ (Lofi HipHop).mp3", "rb") as audio_file:
+    myth.audio(audio_file.read(), format="audio/mp3")
+
 if myth.button("Send balloons!"):
     myth.balloons()
-myth.video(data="https://youtu.be/4in2X_BWUX0?si=P7oU5PQHMU7oGS6W")
+
+myth.video("https://youtu.be/4in2X_BWUX0?si=P7oU5PQHMU7oGS6W")
+
 myth.write(
-    pd.DataFrame(
-        {
-            "first_Coloumn":[1,0,2,3],
-            " :camera: second":[45,56,67,78],
-        }
-    )
+    pd.DataFrame({
+        "first_Coloumn": [1, 0, 2, 3],
+        " :camera: second": [45, 56, 67, 78],
+    })
 )
-import numpy as np
-import altair as alt
 
 df = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 c = (
@@ -38,14 +37,15 @@ c = (
 )
 
 myth.write(c)
+
 if myth.button("pani poliyum neram idhu"):
     myth.snow()
-_LOREM_IPSUM = """
-DANZA KUDURO hello
-"""
+
+_LOREM_IPSUM = "DANZA KUDURO hello"
+
 if myth.button("another one"):
-    mixer.music.load("assests_music\\Danza-Kuduro.mp3")
-    mixer.music.play()
+    with open("assests_music/Danza-Kuduro.mp3", "rb") as audio_file:
+        myth.audio(audio_file.read(), format="audio/mp3")
 
 def stream_data():
     for word in _LOREM_IPSUM.split(" "):
