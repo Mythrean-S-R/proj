8c5de761f00b6a2bcaabbbae61e149ebe4638040
Mythrean-S-R <sit24cs197@sairamtap.edu.in>
2025-04-27 00:42:35 +0530

    img crctd not centered

--- hello.py
+++ hello.py
@@ -15,9 +15,9 @@ with myth.sidebar:
     )
 if selected=="Home":
     myth.markdown(
-    "<hr>",
-        myth.image("assests_music/TEAM_XD.png", width=120),
-    "<img src='assests_music/TEAM_XD.png' width='120'/>"
+    "<hr>",unsafe_allow_html=True)
+    myth.image("assests_music/TEAM_XD.png", width=120)
+    myth.markdown(
     "<h1 style='text-align: center; color: white;'>TEAM XD</h1>"
     "<h4 style='text-align: center;'>-We Build. We Code. We XD. 🚀</h4>""<hr>"
     "<h2>About Us</h2>"
